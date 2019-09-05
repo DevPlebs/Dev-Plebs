278d628ad3c08d388c20f33b199cc180d531c92e
Phil Tietjen <tietjen.philip@gmail.com>
2019-09-04 21:40:12 -0400

    refactor: Update Header to use tailwind styling

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,33 +2,18 @@ import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
 
+import {HeaderWrapper, Container, Brand, BrandLink} from './styles'
+
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
+  <HeaderWrapper>
+    <Container>
+      <Brand>
+        <BrandLink as={Link} to="/">
           {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
+        </BrandLink>
+      </Brand>
+    </Container>
+  </HeaderWrapper>
 )
 
 Header.propTypes = {
