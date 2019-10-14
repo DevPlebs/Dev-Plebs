5ede0234e650ea092e501b7acfb2d62758199448
Phil Tietjen <tietjen.philip@gmail.com>
2019-10-13 21:30:33 -0400

    style: add prettier formatting changes

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,8 +1,8 @@
 import React from "react"
 import { Link } from "gatsby"
 
-import {Layout} from "../components/Layout"
-import {Seo} from "../components/Seo"
+import { Layout } from "../components/Layout"
+import { Seo } from "../components/Seo"
 
 const SecondPage = () => (
   <Layout>
